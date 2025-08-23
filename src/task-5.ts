76d0f544c581017ea70713aaa5d64e7d081acbe0
LEITTO <xxleittoxx@gmail.com>
2025-08-23 23:37:29 +0200

    job done

---
+++ src/task-5.ts
@@ -0,0 +1,13 @@
+console.log("Task 5:");
+
+function logStatus(status: "loading" | "success" | "error") {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
---
+++ src/task-7.ts
@@ -0,0 +1,11 @@
+console.log("Task 7:");
+
+function getMessage(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then(result => console.log(result));
